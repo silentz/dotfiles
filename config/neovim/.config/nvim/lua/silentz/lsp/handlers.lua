5456c990c815677076596763efab32faef013946
Max Pershin <mepershin@gmail.com>
2023-02-28 15:16:21 +0200

    update

--- config/neovim/.config/nvim/lua/silentz/lsp/handlers.lua
+++ config/neovim/.config/nvim/lua/silentz/lsp/handlers.lua
@@ -58,11 +58,6 @@ end
 
 M.on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-        return
-    end
-    illuminate.on_attach(client)
 end
 
 return M
