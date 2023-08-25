800f7ee465527b8eacc0d6f82ec6a45da6ded68e
Max Pershin <mepershin@gmail.com>
2023-08-25 14:37:21 +0300

    update install sh (add polybar

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #/bin/bash
 
 stow -t $HOME i3
+stow -t $HOME polybar
 stow -t $HOME bash
 stow -t $HOME alacritty
 stow -t $HOME nvim
 stow -t $HOME gitui
-
 stow -t $HOME images
-
 stow -t $HOME fonts
 fc-cache -f -v
