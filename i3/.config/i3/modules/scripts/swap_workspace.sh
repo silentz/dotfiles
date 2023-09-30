8f4abfe0c7a131f9459b5e54648304ab906683cd
Max Pershin <mepershin@gmail.com>
2023-09-30 12:57:06 +0300

    add workspace swap

---
+++ i3/.config/i3/modules/scripts/swap_workspace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+move_type=$1
+current=$(i3-msg -t get_workspaces | jq 'map(select(.focused))[0].num')
+origin="$current"
+
+if [[ "$move_type" == "left" || "$move_type" == "up" ]]; then
+    ((current--))
+    if [[ "$current" -lt 1 ]]; then
+        current="1"
+    fi
+else
+    ((current++))
+    if [[ "$current" -gt 10 ]]; then
+        current="10"
+    fi
+fi
+
+i3-msg "rename workspace $current to _temp; \
+        rename workspace $origin to $current; \
+        rename workspace _temp to $origin"
