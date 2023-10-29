e42401a72482be57eb83ec79cd1da3c9c306e56d
Max Pershin <mepershin@gmail.com>
2023-10-29 13:24:26 +0200

    update

--- nvim/.config/nvim/lua/silentz/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/silentz/plugins/null_ls.lua
@@ -38,17 +38,17 @@ return function()
 
         -- NOTE uncomment this to enable autoformatting on buffer write
 
-        -- on_attach = function(client, bufnr)
-        --     if client.supports_method("textDocument/formatting") then
-        --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        --         vim.api.nvim_create_autocmd("BufWritePre", {
-        --             group = augroup,
-        --             buffer = bufnr,
-        --             callback = function()
-        --                 vim.lsp.buf.format({bufnr=bufnr})
-        --             end,
-        --         })
-        --     end
-        -- end,
+        on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({bufnr=bufnr})
+                    end,
+                })
+            end
+        end,
     })
 end
