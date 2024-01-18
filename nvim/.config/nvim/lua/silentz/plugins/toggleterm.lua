bb9715832a87197b3208503c90df949c35856061
Max Pershin <mepershin@gmail.com>
2024-01-18 10:57:44 +0200

    update

--- nvim/.config/nvim/lua/silentz/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/silentz/plugins/toggleterm.lua
@@ -36,7 +36,7 @@ return function()
     vim.cmd('autocmd! TermOpen term://*')
 
     local gitterm = terminal.Terminal:new({
-        cmd = "lazygit",
+        cmd = "gitui",
         hidden = true,
         on_open = function(term)
             local keymaps = myrequire(".keymaps.git")
