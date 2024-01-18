55a940d62b93939f8e2eef4c0eed1d4fab2055de
Max Pershin <mepershin@gmail.com>
2024-01-18 16:22:12 +0200

    fix style

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,7 +3,7 @@
 function dump(o)
     if type(o) == "table" then
         local s = "{ "
-        for k,v in pairs(o) do
+        for k, v in pairs(o) do
             if type(k) ~= "number" then k = '"' .. k .. '"' end
             s = s .. "[" .. k .. "] = " .. dump(v) .. ", "
         end
