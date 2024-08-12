a31a579f9d5f970e98ffd5235c665eaf744061bb
Maxim Pershin <mepershin@gmail.com>
2024-08-12 15:45:13 +0200

    update script

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 stow -t $HOME gtk
 stow -t $HOME script
 stow -t $HOME i3
-stow -t $HOME picom
 stow -t $HOME dunst
 stow -t $HOME rofi
 stow -t $HOME polybar
@@ -13,7 +12,6 @@ stow -t $HOME bash
 stow -t $HOME alacritty
 stow -t $HOME nvim
 stow -t $HOME zed
-stow -t $HOME gitui
 stow -t $HOME lazygit
 stow -t $HOME images
 stow -t $HOME fonts
