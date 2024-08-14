e69907b42007f320edb79b2883ddfb5e3b825b4f
Maxim Pershin <mepershin@gmail.com>
2024-08-14 22:04:03 +0200

    update

--- i3/.config/i3/modules/scripts/statusbar.sh
+++ i3/.config/i3/modules/scripts/statusbar.sh
@@ -5,17 +5,17 @@ screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 primary=$(xrandr --query | grep primary | cut -d" " -f1)
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q lemonbar_generator
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x lemonbar_generator >/dev/null; do sleep 0.1; done
 
 # launch polybar on all screens
 for monitor in $screens; do
     if [[ "$monitor" == "$primary" ]]; then
-        MONITOR="$monitor" polybar -q main &
+        MONITOR="$monitor" statusbar
     else
         # I also want to launch the same bar on second screen
-        MONITOR="$monitor" polybar -q main &
+        MONITOR="$monitor" statusbar
     fi
 done
