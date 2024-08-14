e541cd3e62510c5c8dcc456bb8ef4a693ad37609
Maxim Pershin <mepershin@gmail.com>
2024-08-14 22:04:54 +0200

    remove polybar

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #/bin/bash
 
 stow -t $HOME bin
-
 stow -t $HOME gtk
 stow -t $HOME i3
 stow -t $HOME dunst
 stow -t $HOME rofi
-stow -t $HOME polybar
 stow -t $HOME gsimplecal
 stow -t $HOME bash
 stow -t $HOME alacritty
