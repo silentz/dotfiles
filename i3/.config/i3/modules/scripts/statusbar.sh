9139f7eef9765eba3f187e14dfaab3007c837435
Maxim Pershin <mepershin@gmail.com>
2024-08-14 22:07:15 +0200

    statusbar

--- i3/.config/i3/modules/scripts/statusbar.sh
+++ i3/.config/i3/modules/scripts/statusbar.sh
@@ -13,9 +13,9 @@ while pgrep -u $UID -x lemonbar_generator >/dev/null; do sleep 0.1; done
 # launch polybar on all screens
 for monitor in $screens; do
     if [[ "$monitor" == "$primary" ]]; then
-        MONITOR="$monitor" statusbar
+        MONITOR="$monitor" statusbar &
     else
         # I also want to launch the same bar on second screen
-        MONITOR="$monitor" statusbar
+        MONITOR="$monitor" statusbar &
     fi
 done
