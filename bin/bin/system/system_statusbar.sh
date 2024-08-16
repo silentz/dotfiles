1e3e2700b214b0a55718a12ee71369c6b27917f6
Maxim Pershin <mepershin@gmail.com>
2024-08-16 20:52:22 +0200

    update

--- bin/bin/system/system_statusbar.sh
+++ bin/bin/system/system_statusbar.sh
@@ -4,7 +4,7 @@ killall -q lemonbar_generator
 
 lemonbar_generator | while IFS= read -r line; do
     echo "%{SnEDP-1} $line";
-done | ~/Downloads/bar/lemonbar \
+done | lemonbar \
     -f "StatusbarFont:size=12" \
     -f "Monospace:size=10" \
     -g "x30" \
