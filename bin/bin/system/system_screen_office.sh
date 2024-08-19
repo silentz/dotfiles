a3fb2c632bdd96e246225e5da68fc42984a1a496
Max Pershin <mepershin@gmail.com>
2024-08-19 13:27:50 +0200

    update home and office modes

--- bin/bin/system/system_screen_office.sh
+++ bin/bin/system/system_screen_office.sh
@@ -2,4 +2,4 @@
 
 xrandr --output DP-3-2 --mode 1920x1080 --pos 0x0
 xrandr --output DP-3-3 --mode 1920x1080 --left-of DP-3-2
-xrandr --output eDP-1  --mode 1920x1200 --pos 960x1080
+xrandr --output eDP-1 --off
