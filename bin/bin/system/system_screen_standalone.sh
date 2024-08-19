2a0a3114b78950ba1836bd1108951f9b216dc2d3
Max Pershin <mepershin@gmail.com>
2024-08-19 14:56:32 +0200

    update

--- bin/bin/system/system_screen_standalone.sh
+++ bin/bin/system/system_screen_standalone.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-xrandr --output eDP-1 --mode $(system_default_screen_resolution.sh)
+default_mode=$(system_default_screen_resolution.sh | sed 's/_/x/g')
+xrandr --output eDP-1 --mode "$default_mode"
+
+monitors=$(system_list_monitors.sh)
+for mon in $monitors; do
+    if [ "$mon" != "eDP-1" ]; then
+        xrandr --output "$mon" --off;
+    fi
+done;
