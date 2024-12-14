e7820a284354eab85399b26bfbec222e848babdf
Max Pershin <mepershin@gmail.com>
2024-12-14 19:51:38 +0100

    init yazi config

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #/bin/bash
 
+stow -t $HOME alacritty
+stow -t $HOME bash
 stow -t $HOME bin
+stow -t $HOME dunst
+stow -t $HOME fonts
+stow -t $HOME gsimplecal
 stow -t $HOME gtk
 stow -t $HOME i3
-stow -t $HOME dunst
-stow -t $HOME rofi
-stow -t $HOME gsimplecal
-stow -t $HOME bash
-stow -t $HOME alacritty
-stow -t $HOME zed
-stow -t $HOME vim
 stow -t $HOME images
-stow -t $HOME fonts
+stow -t $HOME rofi
+stow -t $HOME vim
 stow -t $HOME wget
+stow -t $HOME yazi
+stow -t $HOME zed
+
 fc-cache -f -v
